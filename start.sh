a8f18e68191bfbb7ab7e7954dfcf1745de04053d
whoisclebs <clebson.augusto@dcx.ufpb.br>
2024-03-30 21:07:04 -0300

    Add new plugin jars and WorldEdit strings

---
+++ start.sh
@@ -0,0 +1,1 @@
+java -jar spigot-1.20.4.jar -Xmx6G -Xms6G -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M
